860d1a84726956501502f6f2a23e584a16b2aed1
ras0q <ras0q@users.noreply.github.com>
2024-11-16 19:51:14 +0900

    idx_jia_isu_uuid_timestamp

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -26,7 +26,8 @@ CREATE TABLE `isu_condition` (
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY(`id`),
   INDEX (`jia_isu_uuid`),
-  INDEX (`timestamp`)
+  INDEX (`timestamp`),
+  INDEX `idx_jia_isu_uuid_timestamp` (`jia_isu_uuid`, `timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 CREATE TABLE `user` (
