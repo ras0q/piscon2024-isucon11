10d756424787250814e308db5c32acaef9e54b56
ras0q <ras0q@users.noreply.github.com>
2024-11-16 21:24:48 +0900

    Revert "500ms毎"
    This reverts commit ac165d835d98e8c79c022ef907ab4fa9353dd5ab.

--- go/main.go
+++ go/main.go
@@ -1251,7 +1251,7 @@ var conditionsChan = make(chan []IsuCondition, 10000)
 func runPostIsuConditionWorker() {
 	conditionsQueue := make([]IsuCondition, 0, 10000)
 
-	ticker := time.NewTicker(500 * time.Millisecond)
+	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
 	for {
